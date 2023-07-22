0bc3178dd26595f8465e68d2df9bd24558abdb45
GitHub Action <action@github.com>
2023-07-22 01:39:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/baboy/ideaplugincodegen/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/baboy/ideaplugincodegen/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.baboy.ideaplugincodegen.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.baboy.ideaplugincodegen.MyBundle
+import com.github.baboy.ideaplugincodegen.services.MyProjectService
 import javax.swing.JButton
 
 
